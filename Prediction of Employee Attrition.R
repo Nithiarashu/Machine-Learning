9ec936fdaa809cf3b70bf9807fcdf9cafa316c92
Nithiarashu <41011408+Nithiarashu@users.noreply.github.com>
2018-07-11 03:43:18 +0100

    Alternate K-Mean

--- Prediction of Employee Attrition.R
+++ Prediction of Employee Attrition.R
@@ -322,4 +322,125 @@ plot(kmedoidsScores, xlab="k", ylab="DBIndex")
 
 ###################################################################################################
 
+table(my_dataset$Attrition)
+myIris <- numerics[,] #removing the dependent
+myIris <- sapply(myIris, FUN=function(x) { scale(x, scale = T, center=T)})
+res.1 <- kmeans(myIris, 2) #2 is the number of cluster and need to know how to find a better cluster count 
+str(res.1)
+df <- data.frame(cluster = res.1$cluster, Attrition = my_dataset$Attrition)
+table (factor(df$cluster), df$Attrition)
 
+myIris <- numerics[,] #removing the dependent
+myIris <- sapply(myIris, FUN=function(x) { scale(x, scale = T, center=F)})
+res.2 <- kmeans(myIris, 2)
+df <- data.frame(cluster = res.2$cluster, Attrition = my_dataset$Attrition)
+table (factor(df$cluster), df$Attrition)
+
+pca <- prcomp(numerics[,], scale. = T, center = T)
+res.3 <- kmeans(pca$x[, 1:2], 2)
+df <- data.frame(cluster = res.3$cluster, Attrition = my_dataset$Attrition)
+table (factor(df$cluster), df$Attrition)
+
+pca <- prcomp(numerics[,], scale. = T, center = F)
+res.4 <- kmeans(pca$x[, 1:2], 2)
+df <- data.frame(cluster = res.4$cluster, Attrition = my_dataset$Attrition)
+
+table (factor(df$cluster), df$Attrition)
+
+str(res.4)
+totss <- c(res.1[[3]], res.2[[3]], res.3[[3]], res.4[[3]])
+tos.withinss <- c(res.1[[5]], res.2[[5]], res.3[[5]], res.4[[5]])
+betweenss <- c(res.1[[6]], res.2[[6]], res.3[[6]], res.4[[6]])
+performance <- data.frame(totss, tos.withinss, betweenss)
+row.names(performance) <- c("w/o pca +c", "w/o pca", "w pca +c", "w pca")
+performance
+
+plot(iris[,c(1:4)], col=res.1$cluster)
+plot(iris[,c(1:4)], col=res.4$cluster)
+
+
+###################################evaluation - DB index and silhoutte #####################################3
+install.packages("clusterSim")
+library(cluster)
+install.packages("xtable")
+library(xtable)
+install.packages("shiny")
+library(shiny)
+
+library(clusterSim)
+
+library(cluster) #for the silhouette
+results <- list()
+tot.withinss <- c()
+betweenss <- c()
+dbindex <- c()
+silhouettes <- c()
+
+for (k in 2:10) {
+  results[[k]] <- kmeans(myIris, k)
+  tot.withinss[k] <- results[[k]]$tot.withinss
+  betweenss[k] <- results[[k]]$betweenss
+  dbindex[k] <- clusterSim::index.DB(myIris, results[[k]]$cluster, centrotypes="centroids")$DB
+  s <- silhouette(results[[k]]$cluster, daisy(myIris))
+  silhouettes[k] <- mean(s[,3])
+}
+
+par(mfrow=c(2,2))
+plot(tot.withinss, xlab="k")
+plot(betweenss, xlab="k")
+plot(dbindex, xlab="k")
+plot(silhouettes, xlab="k")
+plot(silhouette(results[[3]]$cluster, daisy(myIris)))
+
+
+library(fpc)
+plotcluster(myIris, res.2$cluster)
+
+############################ Outlier treatment using euclidean distance ####################
+res.2$centers #coords of centroids
+centers <- res.2$centers[res.2$cluster, ] #vector of all centers for each point
+distances <- sqrt(rowSums((myIris - centers)^2)) #Euclidean pair-wise distances
+summary(distances)
+sd(distances)
+outliers <- order(distances, decreasing=T)[1:5]
+
+# pick those more than 2 standard deviations from the mean
+outliers <- (distances > (mean(distances) + (2 * sd(distances))) |
+               distances < (mean(distances) - (2 * sd(distances))))
+
+print(myIris[outliers,])
+
+
+# plot clusters (choose 2 dimensions for ease of display)
+plot(myIris[,c("DailyRate.Length", "DistanceFromHome.Width")], pch="o", col=res.2$cluster, cex=0.3)
+# plot cluster centers
+points(res.2$centers[,c("Sepal.Length", "Sepal.Width")], col=1:3, pch=8, cex=1.5)
+# plot outliers
+points(myIris[outliers, c("Sepal.Length", "Sepal.Width")], pch="+", col=4, cex=1.5)
+
+##################Elbow Method for finding the optimal number of clusters
+set.seed(123)
+# Compute and plot wss for k = 2 to k = 15.
+k.max <- 15
+data <- myIris
+wss <- sapply(1:k.max, 
+              function(k){kmeans(data, k, nstart=50,iter.max = 15 )$tot.withinss})
+plot(1:k.max, wss,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of clusters K",
+     ylab="Total within-clusters sum of squares")
+#############################################################################
+
+#K-mediods for including both categorical and numerical in our model
+
+##Aastha code for k-medoids goes here 
+
+#############################Agglomerative Heirarchical ######################################
+d <- dist(myIris, method = "euclidean") # distance matrix
+fit <- hclust(d, method="ward.D")
+plot(fit) # display dendogram
+groups <- cutree(fit, k=3) # cut tree into 3 clusters
+# draw dendogram with red borders around the 3 clusters
+rect.hclust(fit, k=3, border="red")
+
+########################################################################################
